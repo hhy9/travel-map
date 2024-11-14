36d89b542c10999debb818916cae5ca11ef539cf
hhy <hhy@smartlo.net>
2024-11-14 10:50:40 +0900

    eslint설정중

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,3 +1,7 @@
-export default function() {
-    return (<h1>Hello world</h1>)
+export default function () {
+    //eslint 설정 테스트
+  function foo(bar) {
+    console.log("bar");
+  }
+  return <h1>Hello world</h1>;
 }
